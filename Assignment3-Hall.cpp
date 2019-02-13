bbacef95db2f3e4a74c66bcb9053d7ceaff05c1f
Richard Hall <47470944+RichdHall30@users.noreply.github.com>
2019-02-13 06:46:36 -0500

    Add files via upload
    This program allows the user to enter a series of numbers and returns the sum of all the even numbers along with the sum of all the odd numbers after the user enters the escape value of 0. Written for CS 205 at Limestone College

---
+++ Assignment3-Hall.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int even = 0;   //DECLARE VARIABLES
+    int odd = 0;
+    int input;
+
+
+    cout << "This program will show you the sum of entered"             //DECLARE THE PURPOSE
+            " even and odd numbers respectively" << endl;
+    cout << "When finished, enter a '0' (ZERO) for results" << endl;    //Termination instructions
+            cout << endl;
+
+
+    while (input != 0)                              //CONTROL THE LOOP
+    {
+        cout << "Please enter a number: ";          //PROMPT THE USER
+        cin >> input;                               //ACCEPT INPUT
+
+        if (input % 2 == 0)                         //DECIDES EVEN VS. ODD
+        {
+            even = even + input;                    //EVEN NUMBERS ADD UP HERE
+        }
+        else
+        {
+            odd = odd + input;                      //ODD NUMBERS ADD UP HERE
+        }
+    }
+            cout << endl;
+    cout << "The sum of even numbers entered is: " << even << "." << endl; //OUTPUT SUM OF EVEN NUMBERS
+    cout << "The sum of odd numbers entered is: " << odd << "." <<endl; //OUTPUT SUM OF ODD NUMBERS
+
+	return 0;
+}
